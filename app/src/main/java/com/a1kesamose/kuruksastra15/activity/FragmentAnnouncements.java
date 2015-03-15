720c21630fcda4c927fe9b5cf2a0f5036dc02d90
Achyuth Mohan <achyuthm@gmail.com>
2015-03-15 22:34:07 +0530

    Work in progress for Announcement Fragment.

--- app/src/main/java/com/a1kesamose/kuruksastra15/activity/FragmentAnnouncements.java
+++ app/src/main/java/com/a1kesamose/kuruksastra15/activity/FragmentAnnouncements.java
@@ -1,16 +1,22 @@
 package com.a1kesamose.kuruksastra15.activity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.a1kesamose.kuruksastra15.R;
 
 public class FragmentAnnouncements extends Fragment
 {
     private static final String NAVIGATION_DRAWER_POSITION = "navigation_drawer_position";
+    ListView announcementListView;
+    TextView announcementIcon;
+    Typeface fontawesomeTypeface;
 
     public static FragmentAnnouncements newInstance(int navigationDrawerPosition)
     {
@@ -26,6 +32,16 @@ public class FragmentAnnouncements extends Fragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        return inflater.inflate(R.layout.fragment_announcements, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_announcements, container, false);
+
+        fontawesomeTypeface = Typeface.createFromAsset(getActivity().getAssets(),"fonts/fontawesome-webfont.ttf");
+        announcementIcon = (TextView)rootView.findViewById(R.id.announcement_icon);
+        announcementIcon.setTypeface(fontawesomeTypeface);
+
+
+
+        return rootView;
     }
 }
+
+
---
+++ app/src/main/java/com/a1kesamose/kuruksastra15/adapter/AnnouncementListAdapter.java
@@ -0,0 +1,33 @@
+package com.a1kesamose.kuruksastra15.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by Achyuth on 15-Mar-15.
+ */
+public class AnnouncementListAdapter extends BaseAdapter
+{
+
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
